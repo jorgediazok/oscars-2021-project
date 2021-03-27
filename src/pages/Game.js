4221b0b92e29964a384aa80bb119899833a8b54b
Jorge <jorgediazok@gmail.com>
2021-03-27 00:05:05 -0300

    added some css changes

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -46,7 +46,6 @@ const Game = () => {
               <Card
                 image={categoria.fields.image.fields.file.url}
                 pelicula={categoria.fields.movie1}
-                key="movie1"
                 classname={i === currentCategoryIndex ? '' : 'card-hidden'}
                 onClick={onClickHandler}
                 nameVariable={categoria.fields.name1}
@@ -54,7 +53,6 @@ const Game = () => {
               <Card
                 image={categoria.fields.image2.fields.file.url}
                 pelicula={categoria.fields.movie2}
-                key="movie2"
                 classname={i === currentCategoryIndex ? '' : 'card-hidden'}
                 onClick={onClickHandler}
                 nameVariable={categoria.fields.name2}
@@ -62,7 +60,6 @@ const Game = () => {
               <Card
                 image={categoria.fields.image3.fields.file.url}
                 pelicula={categoria.fields.movie3}
-                key="movie3"
                 classname={i === currentCategoryIndex ? '' : 'card-hidden'}
                 onClick={onClickHandler}
                 nameVariable={categoria.fields.name3}
@@ -70,7 +67,6 @@ const Game = () => {
               <Card
                 image={categoria.fields.image4.fields.file.url}
                 pelicula={categoria.fields.movie4}
-                key="movie4"
                 classname={i === currentCategoryIndex ? '' : 'card-hidden'}
                 onClick={onClickHandler}
                 nameVariable={categoria.fields.name4}
@@ -78,7 +74,6 @@ const Game = () => {
               <Card
                 image={categoria.fields.image5.fields.file.url}
                 pelicula={categoria.fields.movie5}
-                key="movie5"
                 classname={i === currentCategoryIndex ? '' : 'card-hidden'}
                 onClick={onClickHandler}
                 nameVariable={categoria.fields.name5}

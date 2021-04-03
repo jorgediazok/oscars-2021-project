4f28d9a43f115135d2f4bdf34943367db123682b
Jorge <jorgediazok@gmail.com>
2021-04-03 18:29:15 -0300

    styling cards part 2

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../styles/Results.css';
 import Winner from '../components/Winner';
+import Oscar from '../images/oscar.png';
 
 const Results = ({ movies }) => {
   console.log(movies);
 
   return (
     <div className="results">
-      <h1 className="results_title">Tus Elegidos</h1>
+      <h1 className="results_title">
+        Tus Elegidos <img src={Oscar} alt="" className="oscar-image" />
+      </h1>
       <div className="containerWinner">
         {movies.map((movie) => (
           <Winner movie={movie} />

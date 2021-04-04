20a25aac4f35916ca4de38cdb31f0c05e120ba27
Jorge <jorgediazok@gmail.com>
2021-04-04 02:56:05 -0300

    added play again button

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,8 +2,11 @@ import React from 'react';
 import '../styles/Results.css';
 import Winner from '../components/Winner';
 import Oscar from '../images/oscar.png';
+import { useHistory } from 'react-router-dom';
 
 const Results = ({ movies }) => {
+  const history = useHistory();
+
   console.log(movies);
 
   return (
@@ -11,6 +14,9 @@ const Results = ({ movies }) => {
       <h1 className="results_title">
         Tus Elegidos <img src={Oscar} alt="" className="oscar-image" />
       </h1>
+      <button className="results__button" onClick={() => history.go(0)}>
+        Jugar Otra Vez
+      </button>
       <div className="containerWinner">
         {movies.map((movie, i) => (
           <Winner movie={movie} key={i} />

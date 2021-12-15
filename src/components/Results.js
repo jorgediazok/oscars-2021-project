f3a5b0a46d35c846324266b34d9ac314055313b2
Jorge <jorgediazok@gmail.com>
2021-12-14 23:56:29 -0300

    trying out without pic - v2

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import '../styles/Results.css';
 import Winner from '../components/Winner';
-import Oscar from '../images/oscar.png';
+// import Oscar from '../images/oscar.png';
 import { useHistory } from 'react-router-dom';
 
 const Results = ({ movies }) => {
   const history = useHistory();
 
   return (
-    <div className="results">
-      <h1 className="results_title">
-        Tus Elegidos <img src={Oscar} alt="" className="oscar-image" />
-      </h1>
-      <button className="results__button" onClick={() => history.go(0)}>
+    <div className='results'>
+      <h1 className='results_title'>Tus Elegidos</h1>
+      <button className='results__button' onClick={() => history.go(0)}>
         Jugar Otra Vez
       </button>
-      <div className="containerWinner">
+      <div className='containerWinner'>
         {movies.map((movie, i) => (
           <Winner movie={movie.pelicula} categoria={movie.categoria} key={i} />
         ))}

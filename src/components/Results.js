0dc8de93a3510750b76eed39064903baf6e69046
Jorge <jorgediazok@gmail.com>
2021-12-15 00:07:54 -0300

    restored images

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/Results.css';
 import Winner from '../components/Winner';
-// import Oscar from '../images/oscar.png';
+import Oscar from '../images/oscar.png';
 import { useHistory } from 'react-router-dom';
 
 const Results = ({ movies }) => {
@@ -9,7 +9,9 @@ const Results = ({ movies }) => {
 
   return (
     <div className='results'>
-      <h1 className='results_title'>Tus Elegidos</h1>
+      <h1 className='results_title'>
+        Tus Elegidos <img src={Oscar} alt='' className='oscar-image' />
+      </h1>
       <button className='results__button' onClick={() => history.go(0)}>
         Jugar Otra Vez
       </button>

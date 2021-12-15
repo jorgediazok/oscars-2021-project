75966c10d388db853b20e4c91d8db9f6e0cc2c46
Jorge <jorgediazok@gmail.com>
2021-12-15 10:45:12 -0300

    changed svg image for png image

--- src/components/Winner.js
+++ src/components/Winner.js
@@ -4,7 +4,14 @@ import '../styles/Winner.css';
 const Winner = ({ movie, categoria }) => {
   return (
     <div className='box'>
-      <img src='/images/oscar2.svg' alt='' className='imageWinner' />
+      <div className='imageWinner-container'>
+        <img
+          src='https://i.ibb.co/StNS9dy/oscar2.png'
+          alt=''
+          className='imageWinner'
+        />
+      </div>
+
       <h2 className='box_categoria'>{categoria}</h2>
       <p className='box_movie'>{movie}</p>
     </div>
